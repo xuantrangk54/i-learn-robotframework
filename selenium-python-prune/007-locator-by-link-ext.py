e134c7f9f3829546ba5b51ff9b0c2ff2d4bbd29a
unknown <xuantrangk54@gmail.com>
2025-10-19 12:18:08 +0700

    :sparkless: locator by link test

---
+++ selenium-python-prune/007-locator-by-link-ext.py
@@ -0,0 +1,19 @@
+import pytest
+import time
+from selenium.common import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+def test_locator_by_name(driver):
+    driver.get('https://i-learn-robotframework-webserver.vercel.app')
+    element = driver.find_element(By.LINK_TEXT, "go to dantri.com.vn")
+    assert element is not None
+
